9769c878ef71df9a3aa5e20e5044a098edf36e1a
linqinhao <976147727@qq.com>
2019-03-01 23:13:50 +0800

    学习网关Zuul的基本使用

---
+++ api-gateway/src/main/java/top/linqinhao/apigateway/filter/LoginFilter.java
@@ -0,0 +1,85 @@
+package top.linqinhao.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * 登录过滤器
+ */
+@Component
+public class LoginFilter extends ZuulFilter {
+
+    /**
+     * 过滤器类型，前置过滤器
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    /**
+     * 过滤器顺序order值越小，越优先执行
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 4;
+    }
+
+    /**
+     * 过滤器是否生效
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        // /apigateway/product/api/v1/product/list
+        System.out.println(request.getRequestURI());
+        // http://localhost:9000/apigateway/product/api/v1/product/list
+        System.out.println(request.getRequestURL());
+
+        // 下单接口
+        String orderSave = "/apigateway/order/api/v1/order/save";
+        if (orderSave.equalsIgnoreCase(request.getRequestURI())){
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 业务逻辑
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+
+        // JWT
+
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        // token对象
+        String token = request.getHeader("token");
+        if (StringUtils.isBlank(token)){
+            token = request.getParameter("token");
+        }
+
+        if (StringUtils.isBlank(token)){
+
+        }else {
+
+        }
+        return null;
+    }
+}
