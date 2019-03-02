398a7d5fba77ca0b59179eb5c6dc738ff9ac934c
linqinhao <976147727@qq.com>
2019-03-02 15:36:25 +0800

    guava接口限流学习

--- api-gateway/src/main/java/top/linqinhao/apigateway/filter/OrderRateLimiterFilter.java
+++ api-gateway/src/main/java/top/linqinhao/apigateway/filter/OrderRateLimiterFilter.java
@@ -1,9 +1,9 @@
 package top.linqinhao.apigateway.filter;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -12,76 +12,49 @@ import javax.servlet.http.HttpServletRequest;
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
 
 /**
- * 登录过滤器
+ * 订单限流
  */
 @Component
-public class LoginFilter extends ZuulFilter {
+public class OrderRateLimiterFilter extends ZuulFilter {
 
-    /**
-     * 过滤器类型，前置过滤器
-     * @return
-     */
+    // 每秒产生1000个令牌
+    private static final RateLimiter RATE_LIMITER = RateLimiter.create(1000);
+
     @Override
     public String filterType() {
         return PRE_TYPE;
     }
 
-    /**
-     * 过滤器顺序order值越小，越优先执行
-     * @return
-     */
     @Override
     public int filterOrder() {
-        return 4;
+        return -4;
     }
 
     /**
-     * 过滤器是否生效
+     * 只对订单接口做限流
      * @return
      */
     @Override
     public boolean shouldFilter() {
         RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletRequest request = requestContext.getRequest();
-
-        // /apigateway/product/api/v1/product/list
-        System.out.println(request.getRequestURI());
-        // http://localhost:9000/apigateway/product/api/v1/product/list
-        System.out.println(request.getRequestURL());
-
-        // 下单接口
-        // ACL
         String orderSave = "/apigateway/order/api/v1/order/save";
         if (orderSave.equalsIgnoreCase(request.getRequestURI())){
             return true;
         }
-
         return false;
     }
 
-    /**
-     * 业务逻辑
-     * @return
-     * @throws ZuulException
-     */
     @Override
     public Object run() throws ZuulException {
 
-        // JWT
-
         RequestContext requestContext = RequestContext.getCurrentContext();
-        HttpServletRequest request = requestContext.getRequest();
-        // token对象
-        String token = request.getHeader("token");
-        if (StringUtils.isBlank(token)){
-            token = request.getParameter("token");
-        }
 
-        // 登录校验逻辑 根据实际情况自定义
-        if (StringUtils.isBlank(token)){
+        if (!RATE_LIMITER.tryAcquire()){
             requestContext.setSendZuulResponse(false);
-            requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+            requestContext.setResponseStatusCode(HttpStatus.TOO_MANY_REQUESTS.value());
         }
+        
         return null;
     }
 }
