e1893f992c91a56ed27784c3e2d9fa8ebf459b9c
qinhaoLin <976147727@qq.com>
2019-03-05 16:36:47 +0800

    学习 Spring Cloud 消息总线Bus整合RabbitMQ

--- product_service/src/main/java/top/linqinhao/product_service/controller/ProductController.java
+++ product_service/src/main/java/top/linqinhao/product_service/controller/ProductController.java
@@ -3,6 +3,7 @@ package top.linqinhao.product_service.controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,14 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@RefreshScope
 public class ProductController {
 
     @Value("${server.port}")
     private String port;
+
+    @Value("${env}")
+    private String env;
 
     @Autowired
     private ProductService productService;
@@ -46,7 +51,7 @@ public class ProductController {
         Product product = productService.findById(id);
         Product result = new Product();
         BeanUtils.copyProperties(product,result);
-        result.setName(result.getName() + " data from port=" + port);
+        result.setName(result.getName() + " data from port=" + port +"  env = "+ env);
         return result;
     }
 }
